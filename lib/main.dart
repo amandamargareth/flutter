43cb5b0de96d178eed2491c32627158ba620e3e2
Amanda Margaretha <amandamargaretha272011@gmail.com>
2024-10-23 11:00:59 +0700

    Add all project files

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TUGAS SESI 3',
+      title: 'TUGAS SESI3',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
